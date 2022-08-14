e402cc64df70ee9d8e038d2c46aac9debdc66532
kimseowoo03 <kimseowoo03@gmail.com>
2022-08-14 20:02:52 +0900

    axois -> fetch로 변경

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ function App() {
   const [reviewValue, setReviewValue] = useState([]);
   const [error, setError] = useState(null);
 
-  const axios = require('axios');
   const url = 'https://react-http-miniproject-default-rtdb.firebaseio.com/userReviewList.json'
 
   const responseReviewList = useCallback(async () => {
     setError(null);
     try {
-      const response = await axios.get(url);
-      const data = await response.data;
+      const response = await fetch(url);
+      const data = await response.json();
       const reviewData = [];
 
       for (const key in data) {
@@ -34,48 +33,40 @@ function App() {
       console.log(` GET ERR >> ${statusCode} / ${statusText}`);
     }
 
-  }, [axios]);
+  }, []);
 
   useEffect(() => {
     responseReviewList();
   }, [responseReviewList]);
 
   const onAdd = async (title, author, review) => {
-    try {
-      const response = await axios.post(url, {
-        title,
-        author,
-        review,
-      });  
-
-       setReviewValue((prev) => [...prev, {
-        id: response.data.name,
-        title,
-        author,
-        review
-      }]);
-
+    try{
+     const response = await fetch(
+      url,
+      {
+         method: "POST",
+         body: JSON.stringify({
+          title: title,
+          author: author,
+          review: review,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=utf-8",
+        },
+      }
+    );
+    const data = await response.json();
+    setReviewValue((prev) => [...prev, {
+      id: data.name,
+      title,
+      author,
+      review
+    }])
     } catch (error) {
-      const errMessage = error.message;
-      const errName = error.name;
-      console.log(` POST ERR >> ${errMessage} / ${errName}`)
+        const errMessage = error.message;
+        const errName = error.name;
+        console.log(` POST ERR >> ${errMessage} / ${errName}`);
     }
-
-    //  await fetch(
-    //   "https://react-http-miniproject-default-rtdb.firebaseio.com/userReviewList.json",
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       title: title,
-    //       author: author,
-    //       review: review,
-    //     }),
-    //     headers: {
-    //       "Content-type": "application/json; charset=utf-8",
-    //     },
-    //   }
-    // );
-    // await responseReviewList(); 
   };
 
   return (
