178774ccfacdd678d95ae0f2a5d139fa3e13e9ee
kimseowoo03 <kimseowoo03@gmail.com>
2022-08-31 16:24:03 +0900

    firebase config .env 환경변수로 뺴기

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import {getFirestore} from "firebase/firestore";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDp8DSXaha7K0GsfIo7KOgNimieV6lzp4c",
-  authDomain: "tutorial-codingapple.firebaseapp.com",
-  projectId: "tutorial-codingapple",
-  storageBucket: "tutorial-codingapple.appspot.com",
-  messagingSenderId: "577125604918",
-  appId: "1:577125604918:web:9381618b8d459365e70297"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
