55ce1adbb095a764566865a616e4684b695d9d56
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-28 00:24:43 +0900

    Feat: 로딩 컴포넌트

---
+++ src/components/UI/Loading.js
@@ -0,0 +1,13 @@
+import classes from "../../styles/Loading.module.scss";
+
+const Loading = () => {
+  return (
+    <div className={classes.loading}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default Loading;
