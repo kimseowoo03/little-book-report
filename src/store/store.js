57ddf7a50698d47229e4cf0ee64210f346c15d7b
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-27 22:05:58 +0900

    Fix: 직렬화문제 해결 및 logger 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import logger from "redux-logger";
+
 import userSlice from "./user-slice";
 import reviewSlice from "./review-slice";
 import uiSlice from "./ui-slice";
 
 const store = configureStore({
   reducer: { review: reviewSlice, ui: uiSlice, user: userSlice },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}).concat(logger),
 });
 
 export default store;
