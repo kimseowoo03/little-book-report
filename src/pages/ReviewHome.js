de476d498c14df74a4e6918bdf575dc0cff75a47
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-28 00:24:27 +0900

    Refactor: ReviewHome 리팩토링 + 로딩 추가

--- src/pages/ReviewHome.js
+++ src/pages/ReviewHome.js
@@ -1,29 +1,32 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // import Notification from "../components/UI/Notification";
 import UserReviewList from "../components/Review/UserReviewList";
+import MyReviewList from "../components/Review/MyReviewList";
 import { userActions } from "../store/user-slice";
 
-import classes from "./ReviewHome.module.css";
+import classes from "../styles/ReviewHome.module.scss";
+import Loading from "../components/UI/Loading";
 
 const ReviewHome = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { loading } = useSelector((state) => state.review);
   const myReview = useSelector((state) => state.user.myReview);
   const user = useSelector((state) => state.user.user);
-  // const errorMessage = useSelector((state) => state.ui.errorMessage);
+
   const reviewWriteHandler = () => {
     if (!user) {
-      navigate("/signup");
+      navigate("/signin");
       alert("로그인해주세요 !");
       return;
     }
     navigate("/write");
   };
-  const reviewHandler = () => {
-    dispatch(userActions.myReviewToggle());
-    navigate("/review", { replace: true });
+
+  const AllReviewHandler = () => {
+    dispatch(userActions.myReviewToggle(false));
   };
 
   const myReviewHandler = () => {
@@ -32,33 +35,35 @@ const ReviewHome = () => {
       alert("로그인해주세요 !");
       return;
     }
-    dispatch(userActions.myReviewToggle());
-    navigate("myreview");
+    dispatch(userActions.myReviewToggle(true));
   };
-  // useEffect(() => {
-  //   //firebase에서 GET
-  //   dispatch(fetchReviewList(userId));
-  // }, [dispatch, userId]);
 
   return (
-    <div className={classes.userReviewForm}>
-      {/* {errorMessage && (
-        <Notification
-          title={errorMessage.title}
-          message={errorMessage.message}
-        />
-      )} */}
-      <div className={classes.reviewBtn}>
-        {myReview ? (
-          <button onClick={reviewHandler}>review</button>
-        ) : (
-          <button onClick={myReviewHandler}>my review</button>
-        )}
-        <button to="/write" onClick={reviewWriteHandler}>
-          write
-        </button>
+    <div className={classes.container}>
+      <div className={classes.content}>
+        {user&&<p>{user.name}님 반가워요!</p>}
+        <div className={classes["button-box"]}>
+          <div className={classes["select-review-box"]}>
+            <button
+              className={!myReview ? classes["selected-review"]: null }
+              onClick={AllReviewHandler}
+            >
+              전체
+            </button>
+            <button
+              className={myReview ? classes["selected-review"]: null}
+              onClick={myReviewHandler}
+            >
+              나의 독후감
+            </button>
+          </div>
+          <button onClick={reviewWriteHandler}>작성하기</button>
+        </div>
       </div>
-      {myReview ? <Outlet /> : <UserReviewList />}
+      <div className={loading?classes.loading: null}>
+      {loading&& <Loading />}
+      </div>
+      {myReview?<MyReviewList />:<UserReviewList />}
     </div>
   );
 };
