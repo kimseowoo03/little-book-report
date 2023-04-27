8695d66ad9f7bcc36c0495ff53a961d01decc6a0
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-27 21:11:14 +0900

    Style: 전체 스타일 배경 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
+import './index.scss';
 import App from "./App";
 import store from "./store/store";
 
