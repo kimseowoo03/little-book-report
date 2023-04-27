24a49bbae0cdcb1eed4e1a069461125f9290ace3
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-27 21:10:49 +0900

    Refactor: App 알맞은 구조로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-import Home from "./pages/Home";
-import UserPageSignUp from "./pages/UserPageSignUp";
 import ReviewHome from "./pages/ReviewHome";
+import SignInPage from "./pages/SignIn";
+import SignUpPage from "./pages/SignUp";
 
 import Header from "./components/UI/Header";
 import UserForm from "./components/Review/UserForm";
-import MyReviewList from "./components/Review/MyReviewList";
+
 import { userActions } from "./store/user-slice";
 
 
 function App() {
-  const userToggle = useSelector((state) => state.user.userToggle);
   const dispatch = useDispatch();
 
   const auth = getAuth();
@@ -38,17 +37,15 @@ function App() {
   }, [auth, dispatch]);
 
   return (
-    <>
+    <div>
       <Header />
       <Routes>
-        {!userToggle && <Route path="/signup" element={<UserPageSignUp />} />}
-        <Route path="/" element={<Home />} />
-        <Route path="review" element={<ReviewHome />}>
-          <Route path="myreview" element={<MyReviewList />} />
-        </Route>
+        <Route path="/" element={<ReviewHome />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
         <Route path="write" element={<UserForm />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
