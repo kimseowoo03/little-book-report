c70ea99d9b866d361c760fd5db43aeb1dee9214f
kimseowoo03 <kimseowoo03@gmail.com>
2023-04-27 21:05:38 +0900

    Fix: Header style과 구조 수정

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,75 +1,50 @@
-import React, { Fragment } from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate, Outlet } from "react-router-dom";
+import { userActions } from "../../store/user-slice";
 
 import { auth } from "../../firebase-config";
 import { signOut } from "firebase/auth";
 
-import { userActions } from "../../store/user-slice";
-import { uiActions } from "../../store/ui-slice";
-
-import classes from "./Header.module.css";
+import classes from "../../styles/Header.module.scss";
+import Button from "./Button";
 
 const Header = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const userToggle = useSelector((state) => state.user.userToggle);
   const user = useSelector((state) => state.user.user);
-  const signToggle = useSelector((state) => state.ui.signToggle);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(userActions.CurrentLoggedInUser(user));
+      } else {
+        dispatch(userActions.CurrentLoggedInUser(null));
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   const logOutHandler = () => {
     signOut(auth);
     navigate("/");
-    dispatch(userActions.userStatusToggle());
-    dispatch(userActions.logOutHandler());
-    signHandler();
-  };
-
-  const signHandler = () => {
-    dispatch(uiActions.signHandler());
   };
 
   return (
-    <Fragment>
-      <div className={classes.header}>
-        <h1>작은 도서관</h1>
-        <nav className={classes.nav}>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="review">감상평</Link>
-            </li>
-          </ul>
-        </nav>
-        {userToggle && (
-          <>
-            <p>{user && user.name}님</p>
-            <button onClick={logOutHandler}>로그아웃</button>
-          </>
-        )}
-        {!userToggle && (
-          <>
-            {signToggle ? (
-              <Link className={classes.homeLink} to="/" onClick={signHandler}>
-                홈으로
-              </Link>
-            ) : (
-              <Link
-                className={classes.homeLink}
-                to="/signup"
-                onClick={signHandler}
-              >
-                {" "}
-                로그인/회원가입
-              </Link>
-            )}
-          </>
-        )}
+    <div className={classes.header}>
+      <div className={classes.content}>
+        <h1>작은 독후감</h1>
+        <div>
+          {user ? (
+            <Button onClick={logOutHandler}>로그아웃</Button>
+          ) : (
+            <Button>
+              <Link to="/signin">로그인</Link>
+            </Button>
+          )}
+        </div>
       </div>
-      <Outlet />
-    </Fragment>
+    </div>
   );
 };
 
